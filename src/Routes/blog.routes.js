7cdc6a867a557761cbaefe889bd41a2086e4dd66
fahadsh01 <fahadshahzad0101@gmail.com>
2025-08-09 03:33:29 +0500

    main

--- src/Routes/blog.routes.js
+++ src/Routes/blog.routes.js
@@ -13,7 +13,7 @@ const blog = Router();
 blog
   .route("/create-blog")
   .post(upload.fields([{ name: "avatar", maxCount: 1 }]), CreateBlog);
-blog.route("/get-blogs").get(verifyjwt, getBlogs);
+blog.route("/get-blogs").get(getBlogs);
 blog.route("/delete-blog/:id").delete(verifyjwt, deleteBlog);
 blog.route("/detailed/:slug").get(getBlogDetail);
 export default blog;
